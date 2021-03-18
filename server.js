07fd1026b259320425164336c0dcda3618a00e5f
ardgery <ardinegery@gmail.com>
2021-03-18 16:38:09 +0700

    add prod config

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ const PORT = process.env.PORT || 5000;
 const server = new ApolloServer({
     typeDefs,
     resolvers,
+    introspection: true,
+    playground: true,
     context: ({ req }) => ({ req, pubsub })
 });
 
